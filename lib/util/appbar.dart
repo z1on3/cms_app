b45eb87e8f0ab7a938952e825b07df24c829c048
Carlo <carlo@fd.com>
2023-10-06 22:32:09 +0800

    agggggggggggggggg

--- lib/util/appbar.dart
+++ lib/util/appbar.dart
@@ -7,41 +7,49 @@ class AppBarNav extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    double maxWidth = 1140; // Your desired maximum width
+    double maxWidth = 1220; // Your desired maximum width
 
     return Container(
-      decoration: const BoxDecoration(color: Colors.black),
+      color: Colors.black,
       height: 80,
       padding: const EdgeInsets.fromLTRB(16, 8, 16, 8),
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Expanded(child: Row()),
-          Text(
-            "TechTown",
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 20,
-              fontFamily: "OpenSans",
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          SizedBox(width: 20),
           Container(
-            width: MediaQuery.of(context).size.width - maxWidth,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                IconButton(
-                  onPressed: () => {login()},
-                  icon: const Icon(Icons.person),
-                  color: Colors.white,
-                  hoverColor: null,
-                ),
-              ],
+            width:
+                MediaQuery.of(context).size.width > maxWidth ? maxWidth : null,
+            child: Expanded(
+              child: Row(
+                children: [
+                  Image.asset('images/playtech-mainsite-logo.webp'),
+                  SizedBox(
+                    width: 20,
+                  ),
+                  TextButton(
+                    onPressed: () => {login()},
+                    child: Text(
+                      "GAMING PC",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontFamily: "OpenSans",
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Expanded(child: Row()),
+                  IconButton(
+                    onPressed: () => {login()},
+                    icon: const Icon(Icons.person),
+                    color: Colors.white,
+                    hoverColor: null,
+                  ),
+                ],
+              ),
             ),
           ),
-          Expanded(child: Row()),
         ],
       ),
     );
